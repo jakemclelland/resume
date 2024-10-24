2f1c6fd36e13e30182f007779002653992d6a44f
Jake McLelland <jake.mclelland@gmail.com>
2024-10-24 14:50:36 -0400

    Corrected some ui glitches and added some experience details

--- src/data/experiences-data.js
+++ src/data/experiences-data.js
@@ -4,16 +4,22 @@ export const experiences = [
       company: "Independent Contractor",
       time: "May 2024 - Present",
       roles: [
-          {
-            id: '8ab8-44ed',
-            title: "DevOps Engineer:",
-            description: "Developed and deployed scalable, containerized microservices using .NET Aspire, Azure Container Apps, and Azure Functions, integrating Service Bus Queues and Topics for event-driven processing. Implemented CI/CD pipelines with Azure DevOps, YAML, and Bicep files to automate building, testing, and deployment of both API, Functions and other serverless components."
-          },
-          {
-            id: 'af99',
-            title: "React Developer:",
-            description: "Developed a responsive personal profile page using React JS, vanilla JavaScript, and pure CSS. Utilized React components to build a modular and maintainable codebase"
-          }
+        {
+          id: 'af99',
+          title: "Web Developer:",
+          description: "Building responsive, modular, and maintainable pages using React JS, JavaScript and CSS"
+        },  
+        {
+          id: '8ab8-44ed',
+          title: "DevOps Engineer:",
+          description: "Developing and deploying scalable microservices using .NET Aspire, Azure Container Apps, and Azure Functions; integrating Service Bus Queues and Topics for event-driven processing. Automating CI/CD pipelines with Azure DevOps, YAML, and Bicep files."
+        },
+        {
+          id: '8ab3-32ab',
+          title: "Database Engineer:",
+          description: "Identifying performance issues using Azure Monitor; optimizing data schema and reducing long running queries to split second results."
+        },
+          
       ],
     },
     {
@@ -24,33 +30,33 @@ export const experiences = [
       roles: [
         {
           id: 'ce7121bc',
-          title: "Senior Web API Specialist:",
+          title: "Web API Specialist:",
           description:
-            "Design and implement web APIs for communication between iOS app and Azure-hosted SQL Server. Collaborate cross-platform functionally for seamless integration. Create, deploy, and schedule Azure Functions for background data processing.",
+            "Designed and implemented APIs to facilitate communication between iOS application and Azure-hosted SQL Server. Used code-first Entity Framework with LINQ syntax. Collaborated with iOS application developers and other cross-functional team members to ensure seamless integration between tech stacks.",
         },
         {
           id: '3876',
           title: "Web Developer:",
           description:
-            "Develop and maintain an admin portal for a retail kiosk company using C# MVC with Razor, Bootstrap and extensive JavaScript. Optimize performance and create complex reports for business analytics.",
+            "Developed and maintained admin portal for retail inventory control using ASP.NET, C#, MVC with Razor, Bootstrap, and JavaScript. Created complex reports, data visualizations and dashboards to deliver actionable insights.",
         },
         {
           id: '4f7b',
           title: "Senior SSIS Developer:",
           description:
-            "Innovate solutions for data integration (ETL) between SQL Server and IBM DB2 mainframe systems. Innovator of proprietary methodologies for data validation, verification, error handling and automated retry/remediation processes. Author, deploy, and schedule ETL jobs for background processing.",
+            "Designed and implemented robust ETL solutions for seamless data integration and synchronization between Azure-hosted SQL Server and IBM DB2 mainframe systems, handling large datasets, including sensitive invoice data. Innovated proprietary methodologies for data validation, verification, error handling, and automated retry processes. Authored, deployed, and scheduled ETL jobs for background processing.",
         },
         {
           id: 'a220',
           title: "Database Administrator:",
           description:
-            "Manage Azure-hosted SQL Server databases, specializing in schema design and index optimization.",
+            "Managed Azure-hosted SQL Server databases, specializing in schema design, data and query optimization.",
         },
         {
           id: '7fa8d784bce0',
           title: "DevOps Engineer:",
           description:
-            "Manage DevOps responsibilities, including building, archiving and deploying iOS app to Apple's AppStoreConnect and TestFlight. Oversee continuous integration/continuous deployment (CI/CD) Azure pipelines to deploy website, API and Azure Functions across staging and production environments. FTP file deployments depending on client site configuration.",
+            "Oversaw continuous integration/continuous deployment (CI/CD) Azure pipelines to deploy website, API and Azure Functions across staging and production environments. Handled iOS app builds, archives, and deployments to Apple’s App Store and TestFlight.",
         },
       ],
     },
@@ -62,33 +68,34 @@ export const experiences = [
       roles: [
         {
           id: 'cc05',
-          title: "Team Facilitator / Scrum Master:",
+          title: "Scrum Master:",
           description:
-            "Led daily stand-up meetings for remote and in-person team members, sprint planning sessions and retrospective meetings aligning with the Agile framework and project scope. Mentor and technology coach. Assisting team members in overcoming roadblocks.",
+            "Team Facilitator for a team of 8 to 12 developers, testers, business analysts. Led daily stand-up meetings, sprint planning sessions and retrospective meetings to ensure efficient project execution and alignment with both Agile methodologies and project scope.",
         },
         {
           id: '4c78',
           title: "Code Reviewer:",
           description:
-            "Provided constructive feedback to adhere to strict code quality and maintainability policies.",
+            "Conducted comprehensive code reviews for all code developed by team members, providing constructive feedback to adhere to strict code quality and maintainability policies.",
         },
         {
           id: '8f14',
           title: "Software Developer:",
           description:
-            "Developed modular WPF desktop applications following the Model-View-ViewModel (MVVM) pattern, leveraging XAML markup to create rich and responsive user interfaces. Implemented application logic and business rules in C# view models, ensuring separation of concerns and testability. Utilized Behavior-Driven Development (BDD) approach, where SpecFlow Gherkin scenarios were used as acceptance criteria, ensuring reliability and robustness of the. Also designed and implemented custom theme in SharePoint intranet on Office 365.",
+            "Developed modular WPF desktop applications following the Model-View-ViewModel (MVVM) pattern, leveraging XAML markup to create rich and responsive user interfaces. Implemented application logic and business rules in C# view models, ensuring separation of concerns and testability. Utilized Behavior-Driven Development (BDD) approach, where SpecFlow Gherkin scenarios were used as acceptance criteria, ensuring reliability and robustness of the. Also designed and implemented a custom theme for SharePoint intranet on Office 365.",
         },
       ],
     },
     {
       id: 'c4edfe14c5d0',
       company: "C2 Systems, USA",
+      location: "Remote",
       time: "November 2016 - November 2017",
       roles: [
         {
           id: 'f05a0575',
           title: "Software Developer:",
-          description: "•	Developed Azure Service Fabric web services and web-based software for loan origination of small business loans for the banking industry. Developed stateful and stateless microservices written in C#.",
+          description: "Developed Azure Service Fabric web services and web-based software for loan origination of small business loans for the banking industry. Developed stateful and stateless microservices written in C#.",
         }
       ],
     },
@@ -96,7 +103,7 @@ export const experiences = [
       id: '47af',
       company: "Humana",
       time: "February 2012 - April 2016",
-      location: "Dallas TX",  
+      location: "Irving, TX",  
       roles: [
         {
           id: '4b04',
@@ -105,28 +112,43 @@ export const experiences = [
             {
               id: '9c24',
               description:
-                "Coordinated development efforts for multiple projects, collaborating with business and product owners to define and document business requirements. Authored solution architectural design documents to guide the team in meeting business objectives. Conducted peer code review and mentored 3 to 7 developers and offsite consultants.",
+                "Lead developer and architect of intranet site using MVC 4.0 with Razor, Bootstrap, C#, and Entity Framework.",
             },
             {
               id: '3768668cacbf',
               description:
-                "Managed the configuration, installation, and deployment of intranet websites to IIS Web Services.",
+                "Coordinated development efforts for multiple projects, collaborating with business and product owners to define and document business requirements.",
             },
             {
               id: '7f0f4264',
               description:
-                "Lead developer and architect of intranet site using MVC 4.0 with Razor, Bootstrap, C#, and Entity Framework.",
+                "Authored solution architectural design documents to guide the team in meeting business objectives.",
             },
             {
               id: '1161',
               description:
-                "Developed SQL Server Integration Services (SSIS) packages for nightly data synchronization between backend systems.",
+                "Conducted peer code review and mentored 3 to 7 developers.",
             },
             {
               id: '45c2',
               description:
-                "Designed and implemented WCF Services for data transmission, utilizing XML serialized C# classes. Also maintained a complex EDI data processor written in C#.",
+                "Managed the configuration, installation, and deployment of intranet websites to IIS Web Services.",
             },
+            {
+              id: '23ab32c',
+              description:
+                "Developed SQL Server Integration Services (SSIS) packages for nightly data synchronization between backend systems.",
+            },
+            {
+              id: '93b27de1f2',
+              description:
+                "Developed WCF Services for real-time and scheduled data transmission, utilizing XML serialized C# classes.",
+            },
+            {
+              id: '638d7e72ba1',
+              description:
+                "Maintained a complex EDI data processor written in C# to support efficient data processing workflows.",
+            }
           ],
         },
       ],
@@ -135,7 +157,7 @@ export const experiences = [
       id: '9375',
       company: "Cypress Software Systems / CRIF Lending Solutions",
       time: "May 2006 - February 2012",
-      location: "Dallas TX",
+      location: "North Richland Hills, TX",
       roles: [
         {
           id: '2c3dffcc5f82',
--- src/data/skills-data.js
+++ src/data/skills-data.js
@@ -1,54 +1,60 @@
 export const skills = [
     {
-      id: '7a99bc647edc',
-      title: "API Development:",
-      description:
-        "Design and implement web APIs using C# and Entity Framework Core, with a focus on optimizing performance.",
-    },
-    {
       id: '9cb2',
-      title: "Web Development:",
+      title: "Software Development:",
       description:
-        "Full-stack web applications development using technologies like ASP.NET Core, MVC, Razor, Bootstrap and JavaScript.",
+        "Full-stack development using technologies like C#, ASP.NET, MVC, Razor, Bootstrap, JavaScript, CSS, and SQL",
     },
     {
-      id: '40c7',
-      title: "Database Management:",
+      id: '7a99bc647edc',
+      title: "Data Integrations:",
       description:
-        "SQL Server and relational database modeling and design, maintenance and optimization.",
-    },
-    {
-      id: '288d',
-      title: "Integration Services:",
-      description:
-        "Proven innovator in SSIS for data synchronization and transmission between systems, as well as error remediation.",
+        "Expert in data and system-to-system integrations, building REST APIs and background data transmission jobs",
     },
     {
       id: '00e30aa8',
       title: "Systems architecture:",
-      description: "Extensive experience developing enterprise-level systems.",
+      description: "Extensive experience developing enterprise-level systems",
     },
     {
       id: 'a02794cae51b',
       title: "DevOps:",
       description:
-        "Build, archive and deploy iOS apps to AppStoreConnect and TestFlight. Manage CI/CD pipelines for web applications, APIs and Azure Functions deployment.",
+        "Azure-based CI/CD pipelines deploying web applications, APIs and Azure Functions, microservices, and Container Apps. Also iOS app store deployments",
+    },
+    {
+      id: '40c7',
+      title: "Database Management:",
+      description:
+        "SQL Server data modeling, schema design, query development, database maintenance, monitoring and optimization",
+    },
+    {
+      id: 'd83ef2b68d',
+      title: "Problem Solving:",
+      description:
+        "Advanced troubleshooting of complex issues that span multiple layers of infrastructure",
+    },
+    {
+      id: 'a332b-38df25bd',
+      title: "Communication:",
+      description:
+        "Frequent public speaker, volunteer NYC MET museum tour guide, and prolific technical documentation author",
     },
     {
       id: 'af38065c',
-      title: "Project Management in Scrum and Agile frameworks:",
+      title: "Project Management:",
       description:
-        "Certified Scrum Master, assisting team members in overcoming roadblocks.",
+        "Scrum and Agile methodologies, Certified Scrum Master, mentoring teams and removing blockers ",
     },
     {
       id: 'b88bc3c36dee',
       title: "Proficient in:",
       description:
-        "C#, SQL Server, ASP.NET, MVC, JavaScript, SSIS, Entity Framework, WCF, .NET Core, Git, GitHub, Jira.",
+        "C#, ASP.NET, JavaScript, Git, GitHub, SQL, MS SQL Server, Azure ",
     },
     {
       id: '822aec2d',
       title: "Familiar with:",
-      description: "React, Swift, SharePoint, Office 365",
+      description: "React, Angular, Blazor, Swift, SharePoint, Office 365 ",
     }
   ];
